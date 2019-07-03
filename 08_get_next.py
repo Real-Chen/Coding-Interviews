e48a3eb6572d1025dc06df93250139e26f0c3e76
Wang Ruichen <15700079221@163.com>
2019-07-03 15:11:42 +0800

    update

---
+++ 08_get_next.py
@@ -0,0 +1,10 @@
+# 面试题8：二叉树的下一个节点
+
+# 二叉树数据结构
+class BinaryTreeNode():
+    def __init__(self, value):
+        self.val = value
+        self.father = None
+        self.left = None
+        self.right = None
+
