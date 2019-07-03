3e726c827f5ee8aad031a55d55ad7f371a2b5a71
Wang Ruichen <15700079221@163.com>
2019-07-03 17:53:44 +0800

    problem 9

--- 09_queue_by_stack.py
+++ 09_queue_by_stack.py
@@ -1,11 +1,18 @@
+# 面试题9： 用两个栈实现队列
+
+# 新建队列类，内部用栈实现
 class queue_by_stack():
     def __init__(self):
+        # python的栈和队列都是用列表实现的
+        # 这里用列表表示栈，规定只能用pop()的操作
         self.stack1 = []
         self.stack2 = []
 
+    # 队尾加值
     def append_tail(self, value):
         self.stack1.append(value)
 
+    # 弹出队首的数据
     def pop_head(self):
         if not self.stack2:
             while self.stack1:
